6e081d0be62bdefd5ec05e81d9b83260dc93697d
ShrekToyta <125920814+ShrekToyta@users.noreply.github.com>
2023-03-01 23:22:09 +0300

    Laba_02

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Классы;
+
+Console.WriteLine("Hello, World!");
+
+//Person x = new Person();
+//x.Read();
+//x.Print();
+
+Console.WriteLine("Input count of persons: ");
+int n = int.Parse(Console.ReadLine());
+
+Person[] Persons = new Person[n];
+
+for (int i = 0; i < n; i++)
+{
+    Persons[i] = new Person();
+    Persons[i].Read();
+}
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine(Persons[i].Surname);
+    Console.WriteLine(Persons[i].Name[0]);
+    Console.WriteLine(Persons[i].Age(Persons[i].BirthDate));
+    Console.WriteLine(Persons[i].Gender);
+}
+

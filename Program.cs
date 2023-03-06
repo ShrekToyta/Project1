ec8313d570678dba24080051a8af0d7b9fa6b05c
Dasha_q_q <125920814+ShrekToyta@users.noreply.github.com>
2023-03-06 21:35:10 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,27 +1,35 @@
-﻿using Классы;
-
-Console.WriteLine("Hello, World!");
-
-//Person x = new Person();
-//x.Read();
-//x.Print();
-
-Console.WriteLine("Input count of persons: ");
-int n = int.Parse(Console.ReadLine());
-
-Person[] Persons = new Person[n];
-
-for (int i = 0; i < n; i++)
+﻿using Khudyakova_Lab_3;
 {
-    Persons[i] = new Person();
-    Persons[i].Read();
-}
+        // зададим 2 вектора
+        Vector vec1 = new Vector(new double[] {2.0 ,3.0, 5.0});
+        Vector vec2 = new Vector(new double[] { 11.0, 2.0, 7.0 });
 
-for (int i = 0; i < n; i++)
-{
-    Console.WriteLine(Persons[i].Surname);
-    Console.WriteLine(Persons[i].Name[0]);
-    Console.WriteLine(Persons[i].Age(Persons[i].BirthDate));
-    Console.WriteLine(Persons[i].Gender);
-}
+        Console.WriteLine("1-ый вектор: " + vec1.ToString());
+        Console.WriteLine("2-ый вектор: " + vec2.ToString());
+        
+        // сумма векторов
+        Vector vec3 = Vector.Add(vec1, vec2);
+        Console.WriteLine("1-ый вектор + 2-ой вектор = " + vec3.ToString());
+        
+        // произведение векторов
+        double dotProduct = Vector.DotProduct(vec1, vec2);
+        Console.WriteLine("1-ый вектор * 2-ой вектор = " + dotProduct);
 
+        //результат проверки двух векторов на равенство (полное совпадение)
+        bool equal = Vector.AreEqual(vec1, vec2);
+        Console.WriteLine("1-ый вектор == 2-ой вектор: " + equal);
+
+        Vector vec4 = new Vector(3);
+        vec4.ReadFromConsole();
+
+
+        Console.WriteLine("4-ый вектор: " + vec4.ToString());
+        Console.WriteLine("Модуль 4-го вектора: " + vec4.GetMagnitude());
+        Console.WriteLine("Макс элемент 4-го вектора: " + vec4.GetMaxElement());
+        Console.WriteLine("Индекс мин элемента 4-го вектора: " + vec4.GetMinElementIndex());
+
+        Vector vec5 = vec4.GetPositiveElementsVector();
+        Console.WriteLine("4-ый вектор: " + vec5.ToString());
+
+        
+}
---
+++ Vector.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Khudyakova_Lab_3
+{
+    internal class Vector
+    {
+        public double[] elem;
+
+        
+        public Vector(int size)
+        {
+            elem = new double[size];
+        }
+
+        public Vector(double[] values)
+        {
+            elem = values;
+        }
+
+        public Vector(Vector vector)
+        {
+            elem = vector.elem;
+        }
+
+        // cвойство для доступа к элементам массива
+        public double this[int index]
+        {
+            get { return elem[index]; }
+            set { elem[index] = value; }
+        }
+
+        // ввод с консоли
+        public void ReadFromConsole()
+        {
+            for (int i = 0; i < elem.Length; i++)
+            {
+                Console.Write($"Введите элемент {i + 1}: ");
+                elem[i] = double.Parse(Console.ReadLine());
+            }
+        }
+
+        // вывод вектора в виде строки
+        public override string ToString()
+        {
+            return string.Join(", ", elem);
+        }
+
+        // метод для вычисления модуля вектора
+        public double GetMagnitude()
+        {
+            return Math.Sqrt(elem.Select(e => e * e).Sum());
+        }
+
+        // метод для получения max элемента вектора
+        public double GetMaxElement()
+        {
+            return elem.Max();
+        }
+
+        // метод для получения индекса min элемента вектора
+        public int GetMinElementIndex()
+        {
+            return Array.IndexOf(elem, elem.Min());
+        }
+
+        // новый вектор, состоящий только из положительных элементов вектора
+        public Vector GetPositiveElementsVector()
+        {
+            return new Vector(elem.Where(e => e > 0).ToArray());
+        }
+
+        // метод для сложения двух векторов
+        public static Vector Add(Vector vec1, Vector vec2)
+        {
+            if (vec1.elem.Length != vec2.elem.Length)
+            {
+                throw new ArgumentException("Векторы должны быть одинаковой длины");
+            }
+
+            double[] result = new double[vec1.elem.Length];
+            for (int i = 0; i < vec1.elem.Length; i++)
+            {
+                result[i] = vec1.elem[i] + vec2.elem[i];
+            }
+
+            return new Vector(result);
+        }
+
+        // метод для вычисления скалярного произведения двух векторов
+        public static double DotProduct(Vector vec1, Vector vec2)
+        {
+            if (vec1.elem.Length != vec2.elem.Length)
+            {
+                throw new ArgumentException("\"Векторы должны быть одинаковой длины");
+            }
+
+            double result = 0;
+            for (int i = 0; i < vec1.elem.Length; i++)
+            {
+                result += vec1.elem[i] * vec2.elem[i];
+            }
+
+            return result;
+        }
+
+        //метод для проверки двух векторов на равенство
+        public static bool AreEqual(Vector vec1, Vector vec2)
+        {
+            if (vec1.elem.Length != vec2.elem.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < vec1.elem.Length; i++)
+            {
+                if (vec1.elem[i] != vec2.elem[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+    }
+}
